f67b21fe7b20aec582f5271c2b7271fa8e2280f3
enricoroselino <roselino.dev@gmail.com>
2025-07-18 14:42:48 +0700

    feat(shared): add serilog logging
    Adds Serilog logging to the `Nebx.Shared` project.  Includes `Serilog` and `Serilog.Sinks.Console` NuGet packages.  A `LoggerHelper` class is created to configure and create the logger instance.  The logger now writes to the console and has a minimum log level override for Microsoft logs.

---
+++ Nebx.Shared/Helpers/LoggerHelper.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace Nebx.Shared.Helpers;
+
+public static class LoggerHelper
+{
+    public static ILogger Create(Action<LoggerConfiguration>? configuration = null)
+    {
+        var loggerConfiguration = new LoggerConfiguration();
+
+        loggerConfiguration.Enrich
+            .FromLogContext();
+
+        loggerConfiguration.WriteTo
+            .Console();
+
+        loggerConfiguration.MinimumLevel
+            .Override("Microsoft", LogEventLevel.Warning);
+
+        configuration?.Invoke(loggerConfiguration);
+        return Log.Logger = loggerConfiguration.CreateLogger();
+    }
+}
