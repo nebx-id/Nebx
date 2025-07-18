890b1b92b0c918a283cb6530bd2610f258b104bb
Enrico Roselino <roselino.dev@gmail.com>
2025-07-18 09:35:56 +0700

    ✅ feat(DDD): Add basic DDD building blocks

---
+++ Nebx.BuildingBlocks.AspNetCore/DDD/DomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using Nebx.BuildingBlocks.AspNetCore.CQRS;
+
+namespace Nebx.BuildingBlocks.AspNetCore.DDD;
+
+public interface IDomainEvent : INotification
+{
+    public Guid EventId { get; }
+    public DateTime OccurredOn { get; }
+    public string EventType { get; }
+}
+
+public abstract record DomainEvent : IDomainEvent
+{
+    public Guid EventId { get; } = Guid.CreateVersion7();
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
---
+++ Nebx.BuildingBlocks.AspNetCore/DDD/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace Nebx.BuildingBlocks.AspNetCore.DDD;
+
+public class DomainException : Exception
+{
+    public DomainException(string message) : base(message)
+    {
+    }
+}
