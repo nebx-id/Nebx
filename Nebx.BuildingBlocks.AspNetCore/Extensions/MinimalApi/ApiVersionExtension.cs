2058c8c78a7b5afa923ac529e978c2ce2ebc7bd6
Enrico Roselino <roselino.dev@gmail.com>
2025-07-18 09:35:17 +0700

    ✅ feat(Nebx.BuildingBlocks.AspNetCore): Add ApiVersionExtension for managing API versions.

---
+++ Nebx.BuildingBlocks.AspNetCore/Extensions/MinimalApi/ApiVersionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Asp.Versioning;
+using Asp.Versioning.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Nebx.BuildingBlocks.AspNetCore.Extensions.MinimalApi;
+
+public static class ApiVersionExtension
+{
+    public static ApiVersionSet CreateVersionSet(this IEndpointRouteBuilder app, params ApiVersion[] apiVersions)
+    {
+        var orderedVersions = apiVersions
+            .Distinct()
+            .OrderBy(v => v.MajorVersion)
+            .ThenBy(v => v.MinorVersion ?? 0);
+
+        var versionSet = app.NewApiVersionSet();
+
+        foreach (var apiVersion in orderedVersions) versionSet.HasApiVersion(apiVersion);
+        return versionSet.ReportApiVersions().Build();
+    }
+}
