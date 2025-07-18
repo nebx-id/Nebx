311df2399014693a30e1be373bd1190a2ae4fe40
Enrico Roselino <roselino.dev@gmail.com>
2025-07-18 09:36:00 +0700

    feat(Nebx.BuildingBlocks.AspNetCore): Add PaginatedData model for pagination.

---
+++ Nebx.BuildingBlocks.AspNetCore/Common/Models/PaginatedData.cs
@@ -0,0 +1,11 @@
+﻿namespace Nebx.BuildingBlocks.AspNetCore.Common.Models;
+
+public static class PaginatedData
+{
+    public static PaginatedData<T> Create<T>(IEnumerable<T> items, int totalCount)
+    {
+        return new PaginatedData<T>(items, totalCount);
+    }
+}
+
+public record PaginatedData<T>(IEnumerable<T> Items, int TotalCount);
