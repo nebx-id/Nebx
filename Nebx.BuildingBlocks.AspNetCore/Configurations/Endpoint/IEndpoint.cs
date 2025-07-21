a5fb6df2749e66bebb6aa9eb171a7ce46eb6be0e
Enrico Roselino <roselino.dev@gmail.com>
2025-07-21 09:11:15 +0700

    docs(IEndpoint): Improves documentation for IEndpoint interface.

--- Nebx.BuildingBlocks.AspNetCore/Configurations/Endpoint/IEndpoint.cs
+++ Nebx.BuildingBlocks.AspNetCore/Configurations/Endpoint/IEndpoint.cs
@@ -2,7 +2,17 @@
 
 namespace Nebx.BuildingBlocks.AspNetCore.Configurations.Endpoint;
 
+/// <summary>
+/// Defines a contract for automatically registering HTTP endpoints.
+/// </summary>
+/// <remarks>
+/// This interface is intended to be used with assembly scanning to discover and register endpoints at application startup.
+/// </remarks>
 public interface IEndpoint
 {
+    /// <summary>
+    /// Adds routes to the application's endpoint route builder.
+    /// </summary>
+    /// <param name="app">The application's endpoint route builder.</param>
     void AddRoutes(IEndpointRouteBuilder app);
 }
