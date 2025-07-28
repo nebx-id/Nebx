85260176af5e3f25145cd2bffacabffa66622994
Enrico Roselino <roselino.dev@gmail.com>
2025-07-28 08:58:05 +0700

    ✅ refactor(ErrorDto): Improve ErrorDto with GuardClauses and ErrorCode

--- Nebx.Shared/Dtos/ErrorDto.cs
+++ Nebx.Shared/Dtos/ErrorDto.cs
@@ -1,11 +1,14 @@
-﻿namespace Nebx.Shared.Dtos;
+﻿using Ardalis.GuardClauses;
+
+namespace Nebx.Shared.Dtos;
 
 public record ErrorDto
 {
     public int StatusCode { get; private set; }
     public string Message { get; private set; } = string.Empty;
-    public string? Path { get; private set; }
-    public string? RequestId { get; private set; }
+    public string Path { get; private set; } = string.Empty;
+    public string RequestId { get; private set; } = string.Empty;
+    public string? ErrorCode { get; private set; }
     public IReadOnlyDictionary<string, string>? Errors { get; private set; }
 
     private ErrorDto()
@@ -16,32 +19,11 @@ public record ErrorDto
     {
         return new ErrorDto()
         {
-            StatusCode = statusCode,
-            Message = message,
-            Path = path,
-            RequestId = requestId,
+            StatusCode = Guard.Against.NegativeOrZero(statusCode, nameof(statusCode)),
+            Message = Guard.Against.NullOrWhiteSpace(message, nameof(message)),
+            Path = Guard.Against.NullOrWhiteSpace(path, nameof(path)),
+            RequestId = Guard.Against.NullOrWhiteSpace(requestId, nameof(requestId)),
         };
-    }
-
-    public static ErrorDto Create(string message, int statusCode)
-    {
-        return new ErrorDto()
-        {
-            StatusCode = statusCode,
-            Message = message,
-        };
-    }
-
-    public ErrorDto SetPath(string path)
-    {
-        Path = path;
-        return this;
-    }
-
-    public ErrorDto SetRequestId(string requestId)
-    {
-        RequestId = requestId;
-        return this;
     }
 
     public void AddErrors(IReadOnlyDictionary<string, string> errors)
@@ -49,5 +31,10 @@ public record ErrorDto
         if (errors.Count == 0) return;
 
         Errors = errors;
+    }
+
+    public void AddErrorCode(string code)
+    {
+        ErrorCode = Guard.Against.NullOrWhiteSpace(code, nameof(code));
     }
 }
