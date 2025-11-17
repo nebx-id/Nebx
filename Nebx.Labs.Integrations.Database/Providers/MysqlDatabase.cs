f3b8472fa0e4ee12eeff62c2f7bba30f59147d02
enricoroselino <roselino.dev@gmail.com>
2025-11-17 11:47:04 +0700

    ✨ feat(database): Implement MySqlDatabase provider
    Adds a new MySqlDatabase class and updates package references.

---
+++ Nebx.Labs.Integrations.Database/Providers/MysqlDatabase.cs
@@ -0,0 +1,28 @@
+using System.Data.Common;
+using MySqlConnector;
+using Nebx.Labs.Integrations.Database.Abstractions;
+
+namespace Nebx.Labs.Integrations.Database.Providers;
+
+/// <inheritdoc />
+public class MySqlDatabase : ISqlDatabase
+{
+    private readonly string _connectionString;
+
+    /// <summary>
+    /// Creates a new <see cref="MySqlDatabase"/> with the specified connection string.
+    /// </summary>
+    /// <param name="connectionString">The Mysql / MariaDB connection string.</param>
+    public MySqlDatabase(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    /// <inheritdoc />
+    public async Task<DbConnection> OpenConnection(CancellationToken cancellationToken = default)
+    {
+        var conn = new MySqlConnection(_connectionString);
+        await conn.OpenAsync(cancellationToken);
+        return conn;
+    }
+}
